d127e3a3a372d6a29b2de2cd258e1396da4731f0
taxionl <1842625771@qq.com>
2022-03-04 20:05:12 +0800

    HW2,b1

---
+++ HW2/b1/prime/prime/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace prime
+{
+    class prime
+    {
+        static void Main(string[] args)
+        {
+
+            bool IsPrime(int i)
+            {
+                bool b = true;
+                if (i == 1 ) b = false;
+                else if(i == 2 ) b = true;
+                else
+                {
+
+                    for (int j = 3; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            b = false;
+                        }
+                    }
+                }
+                return b;
+
+            }
+            Console.WriteLine("请输入数字");
+            string num_=Console.ReadLine();
+            if (num_ == null) return;
+            int num=int.Parse(num_);
+            Console.WriteLine("{0}的素数因子为：/n",num);
+            for(int i = 2; i<= num; i++)
+            {
+                while (num % i == 0)
+                {
+                    if (IsPrime(i))
+                    {
+                        Console.Write("{0} ,",i);
+                    }
+                    num = num / i;
+                }
+            }
+            
+            
+        }
+        
+    }
+}
